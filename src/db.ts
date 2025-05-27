0fe6930bbb42201ef5b9ce61b56f8a3b32e3ac00
Bruno Gianini <blgianini@gmail.com>
2025-05-26 21:27:08 -0300

    initial commit

---
+++ src/db.ts
@@ -0,0 +1,46 @@
+import logger from "./config/logger";
+import { createConnection, getConnectionManager } from "typeorm";
+
+require('reflect-metadata');
+
+export const connectDB = async () => {
+  const connectionManager = getConnectionManager();
+
+  if (connectionManager.has("default")) {
+    const connection = connectionManager.get("default");
+
+    if (!connection.isConnected) {
+      await connection.connect();
+      logger.info("Reconectado ao banco de dados");
+    } else {
+      logger.info("Já conectado ao banco de dados");
+    }
+
+    return connection;
+  }
+
+  // Se ainda não existe conexão, cria uma nova
+
+  logger.debug("Conectando ao banco de dados: " + process.env.DB_HOST)
+
+  const connection = await createConnection({
+    name: "default",
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+
+  logger.info('Conectado ao banco de dados');
+  return connection;
+};
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { connectDB } from './db'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import logger from './config/logger'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(8080, async () => {
+    logger.info("Rodando na porta 8080")
+
+    await connectDB()
+})
